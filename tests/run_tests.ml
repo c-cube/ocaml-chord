d78ed5f94158110696296fb1380c84cd64a23e08
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-08-29 14:37:38 +0200

    newer version of Bencode

--- tests/run_tests.ml
+++ tests/run_tests.ml
@@ -9,7 +9,7 @@ let suite =
 
 let options =
   [ "-lognet", Arg.Set Test_dht.log_net, "enable network logs"
-  ; "-logdht", Arg.Bool (fun b -> Test_dht.log_dht := b), "enable/disable DHT logs"
+  ; "-logdht", Arg.Bool (fun b -> Test_dht.log_dht := b), "enable/disable DHT logs (bool arg)"
   ]
 
 let _ =
